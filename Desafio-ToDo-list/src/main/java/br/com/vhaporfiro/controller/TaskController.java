3ed2f094ad3ff508c7895ea64820b1e804d7bb4f
Victor Hugo <vhaporfiro@gmail.com>
2025-07-24 11:47:53 -0300

    Endpoit getById

--- Desafio-ToDo-list/src/main/java/br/com/vhaporfiro/controller/TaskController.java
+++ Desafio-ToDo-list/src/main/java/br/com/vhaporfiro/controller/TaskController.java
@@ -3,11 +3,14 @@ package br.com.vhaporfiro.controller;
 import br.com.vhaporfiro.entities.Task;
 import br.com.vhaporfiro.service.TaskService;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/task")
@@ -22,5 +25,16 @@ public class TaskController {
     @GetMapping()
     public List<Task> getAllTask() {
         return taskService.getAllTask();
+    }
+
+    @GetMapping("/{Id}")
+    public ResponseEntity<Task> getById(@PathVariable("Id") Long Id) {
+        Optional<Task> task = taskService.getById(Id);
+
+        if (task.isPresent()) {
+            return ResponseEntity.ok(task.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
