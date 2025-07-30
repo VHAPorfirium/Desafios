8b79706fd770f499bbdce368d6d042c4cdfbe3c9
Victor Hugo <vhaporfiro@gmail.com>
2025-07-30 17:18:13 -0300

    Iniciando projeto de encurtador de Url

---
+++ Desafio-EncurtadorUrl/src/main/java/com/vhaporfiro/DesafioEncurtadorUrlApplication.java
@@ -0,0 +1,13 @@
+package com.vhaporfiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEncurtadorUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEncurtadorUrlApplication.class, args);
+	}
+
+}
---
+++ Desafio-EncurtadorUrl/src/test/java/com/vhaporfiro/DesafioEncurtadorUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhaporfiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEncurtadorUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
